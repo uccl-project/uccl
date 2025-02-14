e51f94175130138c9a34852a46a354a74647913b
Pravein-Govindan-Kannan <Pravein.Govindan.Kannan@ibm.com>
2025-02-14 12:54:23 -0800

    Fix copy path of UCCL
    Signed-off-by: Pravein-Govindan-Kannan <Pravein.Govindan.Kannan@ibm.com>

--- rsync.py
+++ rsync.py
@@ -1,13 +1,16 @@
 from shared import *
 import os
+from pathlib import Path
+
 UCCL_HOME = os.getenv("UCCL_HOME")
+uccl_parent_dir = Path(UCCL_HOME).parent if UCCL_HOME else None
 
 def rsync(local_client, nodes):
     wait_handler_vec = []
     for node in nodes:
         wait_handler = exec_command_no_wait(
             local_client,
-            f"rsync -auv -e 'ssh -o StrictHostKeyChecking=no' {UCCL_HOME} {node}:{UCCL_HOME}",
+            f"rsync -auv -e 'ssh -o StrictHostKeyChecking=no' {UCCL_HOME} {node}:{uccl_parent_dir}",
         )
         wait_handler_vec.append(wait_handler)
     for wait_handler in wait_handler_vec:
