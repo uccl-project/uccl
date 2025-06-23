1f99f57fd1704cc0dfccee00241f042aa788e9ef
YangZhou1997 <yangzhou.rpc@gmail.com>
2025-06-23 05:20:33 +0000

    fixing format

--- rdma/transport_test.cc
+++ rdma/transport_test.cc
@@ -392,9 +392,11 @@ static void client_worker(void) {
   }
 }
 
+// clang-format off
 // TO run on AMD HPC cluster:
 // LD_LIBRARY_PATH="${CONDA_LIB_HOME}:/opt/rocm-6.3.1/lib:${LD_LIBRARY_PATH}" HIP_VISIBLE_DEVICES=0 ./transport_test --server=true
 // LD_LIBRARY_PATH="${CONDA_LIB_HOME}:/opt/rocm-6.3.1/lib:${LD_LIBRARY_PATH}" HIP_VISIBLE_DEVICES=0 ./transport_test --serverip=10.0.100.114
+// clang-format on
 
 int main(int argc, char* argv[]) {
   google::InitGoogleLogging(argv[0]);
