cc0ecafa4b4ec6827332460e73cf4bcf07ee4250
CalebZ9909 <138747261+CalebZ9909@users.noreply.github.com>
2025-07-28 22:18:17 -0500

    Delete gpu_driven_amd directory

--- gpu_driven_amd/include/gpu_kernel.hip.h
+++
@@ -1,9 +0,0 @@
-#ifndef GPU_KERNEL_HIP
-#define GPU_KERNEL_HIP
-
-#include "common.hpp"
-#include "ring_buffer.hip.h"
-
-__global__ void gpu_issue_batched_commands(DeviceToHostCmdBuffer* rbs);
-
-#endif  // GPU_KERNEL_HIP
