279adfc7731de28a5a65edd70286df53852d094a
Yang Zhou <yangzhou.rpc@gmail.com>
2025-09-08 18:26:38 -0700

    add target for build (#345)
    * add target for build
    
    * fixing py_version

--- build_and_install.sh
+++ build_and_install.sh
@@ -5,7 +5,7 @@ BUILD_TYPE=${2:-all}
 PY_VER=${3:-$(python3 -c "import sys; print(f'{sys.version_info.major}.{sys.version_info.minor}')")}
 
 if [[ $TARGET != "cuda" && $TARGET != "rocm" ]]; then
-  echo "Usage: $0 [cuda|rocm] [all|rdma|p2p|efa] [PY_VER]" >&2
+  echo "Usage: $0 [cuda|rocm] [all|rdma|p2p|efa|ep] [py_version]" >&2
   exit 1
 fi
 
