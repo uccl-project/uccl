b13336c98a9ebed2d92b4d817069048f18fe8290
MaoZiming <ziming.mao@berkeley.edu>
2025-09-10 07:10:41 +0000

    debug rest of test-low-latency

--- ep/bench/test_low_latency.py
+++ ep/bench/test_low_latency.py
@@ -379,6 +379,8 @@ def test_main(
             return_recv_hook=return_recv_hook,
         )
         large_gemm_with_hook(hook) if return_recv_hook else None
+        buffer.reset_rdma_buffer()
+        time.sleep(1)
 
     print("[simple-test] ✓ All correctness tests passed!", flush=True)
     # Calculate bandwidth
@@ -394,13 +396,12 @@ def test_main(
 
     # Dispatch + combine testing
     # TODO(MaoZiming)
-    if False:
-        avg_t, min_t, max_t = bench(partial(test_func, return_recv_hook=False))
-        print(
-            f"[rank {rank}] Dispatch + combine bandwidth: {(num_dispatch_comm_bytes + num_combine_comm_bytes) / 1e9 / avg_t:.2f} GB/s, "
-            f"avg_t={avg_t * 1e6:.2f} us, min_t={min_t * 1e6:.2f} us, max_t={max_t * 1e6:.2f} us",
-            flush=True,
-        )
+    avg_t, min_t, max_t = bench(partial(test_func, return_recv_hook=False))
+    print(
+        f"[rank {rank}] Dispatch + combine bandwidth: {(num_dispatch_comm_bytes + num_combine_comm_bytes) / 1e9 / avg_t:.2f} GB/s, "
+        f"avg_t={avg_t * 1e6:.2f} us, min_t={min_t * 1e6:.2f} us, max_t={max_t * 1e6:.2f} us",
+        flush=True,
+    )
     if False:
         # Separate profiling
         for return_recv_hook in (False, True):
