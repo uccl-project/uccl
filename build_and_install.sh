767448a7c9dda38e391530ef6bf34ec8b96a0fa6
Ivan Ganev <ivaganev@amd.com>
2025-10-15 16:21:49 -0700

    Update "therock" target to build in a PyPA "manylinux" container (#438)
    * Update "therock" target to build in a PyPA "manylinux" container
    
    Change from gfx arch 4th argument to `build.sh` to a (gfx-specific)
    python index URL argument to simplify upstreaming into TheRock.
    
    Use a PyPA manylinux container as a base image for "therock" builds to
    match TheRock's upstream builds and to maximize compatibility with as
    many distributions as possible. The toolchains in those are specially
    curated for that purpose. The images also come with all python
    versions pre-installed.
    
    Add a 5th argument to `build.sh` & `build_and_install.sh` so a custom
    base image can be specified, if needed.
    
    Add "torch" and "numpy" as dependence for the "uccl[rocm]" installs
    build with TheRock (python packaged ROCm), as opposed to "rocm" which
    is the system-installed ROCm target. UCCL's collective and transfer
    modules need torch and it in turn needs numpy. TheRock project builds
    torch and numpy python packages compatible with python packaged ROCm,
    hence the dependence.
    
    * Update Dockerfile.therock
    
    Use the actual current TheRock base image. The PyPA image it is based on is in the comment above.
    
    ---------
    
    Co-authored-by: Yang Zhou <yangzhou.rpc@gmail.com>

--- build_and_install.sh
+++ build_and_install.sh
@@ -3,15 +3,18 @@
 TARGET=${1:-cuda}
 BUILD_TYPE=${2:-all}
 PY_VER=${3:-$(python3 -c "import sys; print(f'{sys.version_info.major}.{sys.version_info.minor}')")}
-GFX_VER=${4:-gfx94X-dcgpu}
+# The default for ROCM_IDX_URL depends on the gfx architecture of your GPU and the index URLs may change.
+ROCM_IDX_URL=${4:-https://rocm.nightlies.amd.com/v2/gfx94X-dcgpu}
+# The default for THEROCK_BASE_IMAGE is current, but may change. Make sure to track TheRock's dockerfile.
+THEROCK_BASE_IMAGE=${5:-quay.io/pypa/manylinux_2_28_x86_64@sha256:d632b5e68ab39e59e128dcf0e59e438b26f122d7f2d45f3eea69ffd2877ab017}
 
 if [[ $TARGET != cuda* && $TARGET != rocm* && $TARGET != "therock" ]]; then
-  echo "Usage: $0 [cuda|rocm|therock] [all|rdma|p2p|efa|ep] [py_version] [rocm_index_url]" >&2
+  echo "Usage: $0 [cuda|rocm|therock] [all|rdma|p2p|efa|ep] [py_version] [rocm_index_url] [therock_base_image]" >&2
   exit 1
 fi
 
 ARCH_SUFFIX=$(uname -m)
-./build.sh $TARGET $BUILD_TYPE $PY_VER $GFX_VER
+./build.sh $TARGET $BUILD_TYPE $PY_VER $ROCM_IDX_URL $THEROCK_BASE_IMAGE
 pip install -r requirements.txt
 pip uninstall uccl -y || true
 if [[ $TARGET != "therock" ]]; then
@@ -19,5 +22,5 @@ if [[ $TARGET != "therock" ]]; then
 else
   # TheRock packages ROCm dependences through python packaging
   # That (currently) requires --extra-index-url
-  pip install --extra-index-url https://rocm.nightlies.amd.com/v2/${GFX_VER} $(ls wheelhouse-$TARGET/uccl-*.whl)[rocm]
+  pip install --index-url ${ROCM_IDX_URL} $(ls wheelhouse-$TARGET/uccl-*.whl)[rocm]
 fi
