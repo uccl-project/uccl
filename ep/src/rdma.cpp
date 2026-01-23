512a9ae7f742bb4b7a4a1d03330c30971578ca2d
YangZhou1997 <yangzhou.rpc@gmail.com>
2026-01-23 07:53:41 +0000

    using all 4 nics

--- ep/src/rdma.cpp
+++ ep/src/rdma.cpp
@@ -155,10 +155,9 @@ void per_thread_rdma_init(ProxyCtx& S, void* gpu_buf, size_t bytes, int rank,
 #ifdef EFA
     int num_efas = 0;
 #endif
-for (auto& p : dist) {
+    for (auto& p : dist) {
 #ifdef EFA
-      if (strncmp(p.first.c_str(), "rdmap", 5) == 0)
-        num_efas++;
+      if (strncmp(p.first.c_str(), "rdmap", 5) == 0) num_efas++;
       if (p.second == min_d && strncmp(p.first.c_str(), "rdmap", 5) == 0)
         candidates.push_back(p.first);
 #else
@@ -177,12 +176,12 @@ for (auto& p : dist) {
 #ifdef EFA
       // NOTE(MaoZiming): This is a temporary hack.
       if (num_efas == 32) {
-        // On p5, there are 8 NICs with the same distance.
-        auto half = (local_rank % 2) * 4;
-        // GPU0 uses candidates[0/1/2/3], GPU1 uses candidates[4/5/6/7], etc.
-        selected_nic_name = candidates[thread_idx % 4 + half];
+        assert(candidates.size() == 4);
+        // On p5, there are 4 NICs with the same distance.
+        selected_nic_name = candidates[thread_idx % 4];
         use_ll_sl = true;
       } else if (num_efas == 16) {
+        assert(candidates.size() == 4);
         // On p5e/p5en, there are 4 NICs with the same distance.
         // We hardcode the first half Proxies to use the first NIC, and the
         // second half to use the second NIC.
@@ -193,6 +192,7 @@ for (auto& p : dist) {
       } else {
         // On p6-b200, there is 2 NICs with the same distance.
         assert(num_efas == 8);
+        assert(candidates.size() == 2);
         auto half = (local_rank % 2) * 1;
         selected_nic_name = candidates[thread_idx % 1 + half];
         use_ll_sl = true;
