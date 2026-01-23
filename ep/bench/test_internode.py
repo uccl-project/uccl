a8869c4db12887842bd54c25115ab31bf82e050e
YangZhou1997 <yangzhou.rpc@gmail.com>
2026-01-23 08:05:39 +0000

    nits

--- ep/bench/test_internode.py
+++ ep/bench/test_internode.py
@@ -391,8 +391,6 @@ def test_main(
                     dispatch_bf16_nvl_recv_bytes = recv_x.numel() * 2
                     combine_bf16_nvl_send_bytes = dispatch_bf16_nvl_recv_bytes
                     combine_bf16_rdma_recv_bytes = dispatch_bf16_rdma_send_bytes
-                    
-                    torch.cuda.synchronize()
     if local_rank == 0:
         print("", flush=True)
 
